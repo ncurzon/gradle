692f3dd94de7c45490ca3b1a6d4a411fde6bdb1b
teyckmans <teyckmans@gmail.com>
2009-01-21 22:14:16 +0100

    classpath startup issues resolved

---
+++ gradle-api/src/main/groovy/org/gradle/bundle/Activator.java
@@ -0,0 +1,11 @@
+package org.gradle.bundle;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: teyckmans
+ * Date: Jan 21, 2009
+ * Time: 9:06:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Activator {
+}
